aebb48b5d26bb7f57eff607904522240ba09c7fd
Tuan Luong <tdluong@crimson.ua.edu>
2022-12-10 13:43:01 -0600

    added deviation controller. need tuning

--- mat_converter/log/malt_log_analyzer.m
+++ mat_converter/log/malt_log_analyzer.m
@@ -2,13 +2,15 @@ close all
 clear
 clc
 
-load malt24.mat;
+load malt29.mat;
 
 filtered_vbx = vms_aux7;
 test_wp = [2 1 0;
            6 1 0;
-           6 3 0;
-           2 3 0];
+           6 2 0;
+           2 2 0;
+           2 3 0;
+           6 3 0];
 cage_coor_lla = [33.2154770, -87.5436600, 0];
 mav_lla = [rad2deg(ext_gnss1_lat_rad), rad2deg(ext_gnss1_lon_rad), zeros(size(ext_gnss1_lat_rad))];
 mav_pos_ned = lla2ned(mav_lla, cage_coor_lla,'flat');
@@ -30,17 +32,18 @@ grid on
 grid minor
 legend
 
-% figure(2)
-% plot (sys_time_s,vms_aux8,'DisplayName','vb y cmd')
-% hold on
-% plot(sys_time_s, vms_aux9,'DisplayName','vb y')
-% grid on
-% grid minor
-% legend
+figure(2)
+plot (sys_time_s,vms_aux8,'DisplayName','vb y cmd')
+hold on
+plot(sys_time_s, vms_aux9,'DisplayName','vb y')
+grid on
+grid minor
+legend
 
-% figure(3)
-% plot (test_wp(:,1), test_wp(:,2),'*');
-% hold on
-% plot (mav_pos_ned(:,1), mav_pos_ned(:,2))
-% xlim([0 8])
-% ylim([0 5])
+figure(3)
+plot (test_wp(:,1), test_wp(:,2),'*');
+hold on
+plot (test_wp(:,1), test_wp(:,2),'b');
+plot (mav_pos_ned(:,1), mav_pos_ned(:,2),'r')
+xlim([0 8])
+ylim([0 5])
